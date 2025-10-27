c343bb3963a810aa7764324621dc56eb06cebf63
Juliahelm-18 <juliahelmrf@gmail.com>
2025-10-26 23:50:00 -0300

    teste

--- cozinha/pedidos/pedido.py
+++ cozinha/pedidos/pedido.py
@@ -8,6 +8,10 @@ class Pedido:
     def __init__(self, duracao_pedido: int, tipo_pedido: str):
         self.__duracao_pedido = duracao_pedido
         self.__tipo_pedido = tipo_pedido
+        if tipo_pedido == Pedido.TIPO_PRIORITARIO:
+            Pedido.qt_total_pedidos_prioritarios += 1
+        else:
+            Pedido.qt_total_pedidos_normais += 1
 
     def consome_duracao(self) -> None:
         self.__duracao_pedido -= 1
@@ -19,13 +23,9 @@ class Pedido:
         return self.__duracao_pedido
 
     def eh_prioritario(self) -> bool:
-        if self.__tipo_pedido == Pedido.TIPO_PRIORITARIO:
-            Pedido.qt_total_pedidos_prioritarios += 1
         return self.__tipo_pedido == Pedido.TIPO_PRIORITARIO
 
     def eh_normal(self) -> bool:
-        if self.__tipo_pedido == Pedido.TIPO_NORMAL:
-            Pedido.qt_total_pedidos_normais += 1
         return self.__tipo_pedido == Pedido.TIPO_NORMAL
 
     def __str__(self) -> str:
--- tests/test_estatistica.py
+++ tests/test_estatistica.py
@@ -1,4 +1,5 @@
 import unittest
+from cozinha.pedidos.pedido import Pedido
 from cozinha.restaurante import Restaurante
 from cozinha.estatistica.estatistica import Estatistica
 from cozinha.pedidos.pedido_normal import PedidoNormal
@@ -6,18 +7,25 @@ from cozinha.pedidos.pedido_prioritario import PedidoPrioritario
 
 class TestEstatistica(unittest.TestCase):
 
+    def setUp(self):
+        Pedido.qt_total_pedidos_normais = 0
+        Pedido.qt_total_pedidos_prioritarios = 0
+
+        self.restaurante = Restaurante(qtd_cozinheiros=2, limite_fila_pedidos=5)
+        
+        self.estatistica = Estatistica(self.restaurante)
+
     def test_calculo_estatisticas(self):
         """Verifica se as estatísticas refletem corretamente os pedidos e cozinheiros."""
-        restaurante = Restaurante(qtd_cozinheiros=2, limite_fila_pedidos=5)
         p1 = PedidoNormal(1)
         p2 = PedidoPrioritario(1)
-        restaurante.novo_pedido(p1)
-        restaurante.novo_pedido(p2)
+        self.restaurante.novo_pedido(p1)
+        self.restaurante.novo_pedido(p2)
 
-        restaurante.executa_pedidos()
-        restaurante.executa_pedidos()
+        self.restaurante.executa_pedidos()
+        self.restaurante.executa_pedidos()
 
-        est = Estatistica(restaurante)
+        est = Estatistica(self.restaurante)
         self.assertEqual(est.total_pedidos(), 2)
         self.assertEqual(est.total_concluidos(), 2)
         self.assertIn(est.cozinheiro_que_mais_atendeu(), [1,2]) 
