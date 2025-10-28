8410de80e6e0119ca86aa778ed39d7e4773a67ba
Júlia Helm <juliahelmrf@gmail.com>
2025-10-27 22:32:22 -0300

    Update estatistica.py

--- cozinha/estatistica/estatistica.py
+++ cozinha/estatistica/estatistica.py
@@ -1,3 +1,4 @@
+# Métodos para coletar estatísticas do restaurante
 from cozinha.restaurante import Restaurante
 from cozinha.pedidos.pedido_normal import PedidoNormal
 from cozinha.pedidos.pedido_prioritario import PedidoPrioritario
@@ -7,6 +8,21 @@ class Estatistica:
 
     def __init__(self, restaurante: Restaurante):
         self.restaurante = restaurante
+
+    def __str__(self):
+        return (
+        "Estatísticas do restaurante:\n"
+        f"- Total de pedidos recebidos: {self.total_pedidos()}\n"
+        f"   Prioritários: {self.total_pedidos_recebidos_prioritarios()}\n"
+        f"   Normais: {self.total_pedidos_recebidos_normais()}\n"
+        f"- Pedidos concluídos: {self.total_concluidos()}\n"
+        f"   Prioritários: {self.total_concluidos_prioritarios()}\n"
+        f"   Normais: {self.total_concluidos_normais()}\n"
+        f"- Pedidos rejeitados: {self.total_rejeitados()}\n"
+        f"   Prioritários: {self.total_rejeitados_prioritarios()}\n"
+        f"   Normais: {self.total_rejeitados_normais()}\n"
+    )
+
 
     def total_pedidos_recebidos_prioritarios(self) -> int:
         return Pedido.qt_total_pedidos_prioritarios
@@ -36,18 +52,22 @@ class Estatistica:
     def total_concluidos(self) -> int:
         return self.restaurante.contador_pedidos_concluidos
     
+    # Retorna uma lista com o número de pedidos atendidos por cada cozinheiro
     def pedidos_atendidos_por_cozinheiro(self):
         lista = []
         for c in self.restaurante.cozinheiros:
-            lista.append(c.get_pedidos_atendidos())
+            lista.append(c.pedidos_concluidos())
         return lista
-
+    
+    # Retorna uma lista com o tempo ocioso de cada cozinheiro
     def tempo_ocioso_por_cozinheiro(self):
         lista = []
         for c in self.restaurante.cozinheiros:
-            lista.append(c.get_tempo_ocioso())
+            lista.append(c.tempo_livre())
         return lista
 
+    # Retorna o número do cozinheiro que mais atendeu pedidos 
+    # Contagem começa em 1
     def cozinheiro_que_mais_atendeu(self):
         atendimentos = self.pedidos_atendidos_por_cozinheiro()
         if len(atendimentos) == 0:
